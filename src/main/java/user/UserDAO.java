ecac1af6b5851e5cdc274308bd32a89abe7e56e7
wingardium99 <94370483+wingardium99@users.noreply.github.com>
2022-08-01 10:10:19 +0900

    Update UserDAO.java
    UserDAO = 실질적으로 데이터베이스에 접근하는 것.

--- src/main/java/user/UserDAO.java
+++ src/main/java/user/UserDAO.java
@@ -7,8 +7,8 @@ import java.sql.ResultSet;
 
 public class UserDAO {
 
-	private Connection conn;
-	private PreparedStatement pstmt;  // 특정sql문장 수행하도록 하는 class
+	private Connection conn; // 커넥션 객체
+	private PreparedStatement pstmt;  // 특정db에 sql문장 수행하도록 하는 class
 	private ResultSet rs;  // sql 문장 수행이후 나온 결과값에 대해 처리하는 class
 	
 	public UserDAO() {
@@ -26,10 +26,10 @@ public class UserDAO {
 	public int login(String userID, String userPassWord) {
 		String SQL = "SELECT userPassWord FROM USER WHERE userID = ?";
 		try {
-			pstmt = conn.prepareStatement(SQL);
-			pstmt.setNString(1, userID);
-			rs = pstmt.executeQuery();
-			if (rs.next()) {
+			pstmt = conn.prepareStatement(SQL); // conn 객체에서 prepareStatement를 실행하도록 준비
+			pstmt.setNString(1, userID); // sql 문장에 ? 값을 넣음
+			rs = pstmt.executeQuery(); //실제로 SQL 문장을 데이터베이스에서 실행시킨 후 그 값을 rs에 담아준다.
+			if (rs.next()) { // 검색이 되면 true
 				if(rs.getNString(1).contentEquals(userPassWord)) {
 					return 1; //로그인 성공
 				}
